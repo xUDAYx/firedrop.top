6604e957016e87f21d5c05e7e6d3936cd0e5f73b
Uday Jawheri <jawheriuday@gmail.com>
2024-07-06 17:36:46 +0530

    mobile view fix

--- app/(root)/(routes)/projects/page.tsx
+++ app/(root)/(routes)/projects/page.tsx
@@ -8,27 +8,35 @@ export default function ProjectsPage() {
 
   return (
     <div>
-      <div className=' mb-8 mt-20'>
-        <h1 className='text-center text-white text-5xl mb-1 font-bold'>PROJECTS</h1>
+      <div>
+        {/* Heading */}
+        <h1 className='text-center text-white text-5xl mb-1 mt-24 font-bold'>PROJECTS</h1>
         <p className='text-center my-0 text-gray3'>Get your project done in a day not months</p>
+
+
+        {/* gray bar  */}
         <div className="mx-auto w-24 h-1 my-12 bg-gradient-to-r from-[#464f57] to-[#6c7882] rounded-full"></div>
-        <div>
-        <ul className="flex flex-wrap justify-items-center gap-4 px-14">
-          {allProjectData.map((project: any) => (
-            <ProjectCard
-              key={project.slug}
-              project={{
-                slug: project.slug,
-                imageUrl: project.imageUrl,
-                title: project.title,
-                description: project.description,
-                price: project.price,
-                tags: project.tags
-              }}
-            />
-          ))}
-        </ul>
-      </div>
+
+
+        <div className="">
+          <div className="bg-blue-300">
+            <ul>
+              {allProjectData.map((project: any) => (
+                <ProjectCard
+                  key={project.slug}
+                  project={{
+                    slug: project.slug,
+                    imageUrl: project.imageUrl,
+                    title: project.title,
+                    description: project.description,
+                    price: project.price,
+                    tags: project.tags
+                  }}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
