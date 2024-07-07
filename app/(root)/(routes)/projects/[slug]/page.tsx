3c8928ec0e72378e76f71fa9fdd238014e37c895
Uday Jawheri <jawheriuday@gmail.com>
2024-07-07 22:47:05 +0530

    buy cards

--- app/(root)/(routes)/projects/[slug]/page.tsx
+++ app/(root)/(routes)/projects/[slug]/page.tsx
@@ -1,90 +1,182 @@
-import React from 'react';
-import { Footer } from '@/components/footer';
-import ScrollUp from '@/components/ui/scroll-up';
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import ReactMarkdown from 'react-markdown';
-import remarkHtml from 'remark-html';
+import React from 'react'
+import { Footer } from '@/components/footer'
+import ScrollUp from '@/components/ui/scroll-up'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import ReactMarkdown from 'react-markdown'
+import remarkHtml from 'remark-html'
+import { Heading3 } from 'lucide-react'
 
 interface ProjectData {
   frontmatter: {
-    title: string;
-    description: string;
-    imageUrl: string;
-    headline: string;
-  };
-  content: string;
+    title: string
+    description: string
+    imageUrl: string
+    headline: string
+    price: number
+  }
+  content: string
 }
 
 interface ProjectPageProps {
   params: {
-    slug: string;
-  };
+    slug: string
+  }
 }
 
 const ProjectPage: React.FC<ProjectPageProps> = ({ params }) => {
-  const { slug } = params;
-  const projectDir = path.join(process.cwd(), 'projects', slug);
-  const filePath = path.join(projectDir, '_index.md');
+  const { slug } = params
+  const projectDir = path.join(process.cwd(), 'projects', slug)
+  const filePath = path.join(projectDir, '_index.md')
 
-  let projectData: ProjectData | null = null;
+  let projectData: ProjectData | null = null
 
   try {
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const { data, content } = matter(fileContents);
+    const fileContents = fs.readFileSync(filePath, 'utf8')
+    const { data, content } = matter(fileContents)
 
     projectData = {
       frontmatter: {
         title: data.title,
         description: data.description,
-        imageUrl: `${slug}/img/cover.webp`,
+        imageUrl: `/projects/${projectDir}/img/cover.webp`,
         headline: data.headline,
+        price: data.price
       },
-      content: content,
-    };
+      content: content
+    }
   } catch (error: any) {
     if (error.code === 'ENOENT') {
-      console.log(`No _index.md file found for project: ${slug}`);
+      console.log(`No _index.md file found for project: ${slug}`)
     } else {
-      console.error(`Error reading project file: $app\(root)\(routes)\projects\[slug]\page.tsx`, error);
+      console.error(
+        `Error reading project file: $app\(root)\(routes)\projects\[slug]\page.tsx`,
+        error
+      )
     }
   }
 
   if (!projectData) {
-    return <div>Project not found</div>;
+    return <div>Project not found</div>
   }
 
   return (
     <>
-      <div className="max-w-3xl mx-auto text-white">
-        <h1 className="text-center text-6xl font-extrabold mt-10">{projectData.frontmatter.title}</h1>
-        <p className="text-gray-300 my-1 text-center">{projectData.frontmatter.description}</p>
+      <div className="max-w-3xl p-2 mx-auto text-white">
+        <h1 className="text-center text-6xl font-extrabold mt-10">
+          {projectData.frontmatter.title}
+        </h1>
+        <p className="text-gray-300 my-1 text-center">
+          {projectData.frontmatter.description}
+        </p>
 
         <div className="mx-auto w-24 h-1 my-24 bg-gradient-to-r from-[#464f57] to-[#6c7882] rounded-full"></div>
 
-        <img src={projectData.frontmatter.imageUrl} alt={projectData.frontmatter.title} />
+        <img
+          src={projectData.frontmatter.imageUrl}
+          alt={projectData.frontmatter.title}
+        />
 
         <div className="mx-auto w-24 h-1 my-24 bg-gradient-to-r from-[#f97316] to-[#ec4899] rounded-full"></div>
 
         <div className="mb-14">
-          <ReactMarkdown remarkPlugins={[remarkHtml]}>{projectData.frontmatter.headline}</ReactMarkdown>
+          <ReactMarkdown remarkPlugins={[remarkHtml]}>
+            {projectData.frontmatter.headline}
+          </ReactMarkdown>
         </div>
 
-        <ReactMarkdown remarkPlugins={[remarkHtml]} components={{
-            h1: (props) => <h1 className="text-4xl font-bold mb-8 mt-16" {...props} />,
-            h2: (props) => <h2 className="text-3xl font-bold  mb-8 mt-16" {...props} />,
-            h3: (props) => <h3 className="text-2xl font-bold text-[#d1d5db]  mb-8 mt-16" {...props} />,
-            li: (props) => <li className="my-2 pl-1 ml-8 text-[#d1d5db] list-disc" {...props} />,
-            p: (props) => <p className="my-2  text-[#d1d5db] list-disc" {...props} />,
-          }}>
+        <ReactMarkdown
+          remarkPlugins={[remarkHtml]}
+          components={{
+            h1: (props) => (
+              <h1
+                className="text-4xl font-bold mb-8 mt-16 uppercase"
+                {...props}
+              />
+            ),
+            h2: (props) => (
+              <h2
+                className="text-3xl font-bold mb-8 mt-16 uppercase"
+                {...props}
+              />
+            ),
+            h3: (props) => (
+              <h3
+                className="text-2xl font-bold text-[#d1d5db] mb-8 mt-16"
+                {...props}
+              />
+            ),
+            li: (props) => (
+              <li
+                className="my-2 pl-1 ml-8 text-[#d1d5db] list-disc"
+                {...props}
+              />
+            ),
+            p: (props) => (
+              <p className="my-2 text-[#d1d5db] list-disc" {...props} />
+            ),
+            // Add a custom component for the update tag
+            span: (props) => {
+              const { node } = props
+              if (
+                node &&
+                node.properties &&
+                node.properties.className &&
+                typeof node.properties.className === 'string' &&
+                node.properties.className.includes('tag-pro')
+              ) {
+                return <span className="tag tag-sm tag-pro" {...props} />
+              }
+              return <span {...props} />
+            }
+          }}
+        >
           {projectData.content}
         </ReactMarkdown>
       </div>
+      <div className="max-w-3xl mx-auto px-10 text-center">
+        <h2 className="text-5xl font-extrabold text-white pb-2 inline-block mt-24 mb-12">
+          HOW TO BUY
+        </h2>
+        <div className="mx-auto w-24 h-1 mt-0 mb-24 bg-gradient-to-r from-purple-600 to-purple-400 rounded-full"></div>
+
+        <div className=" cursor-pointer bg-slate-900 rounded-lg shadow-3xl p-8 max-w-sm mx-auto border-blue-500 border border-solid ">
+          <p className="text-[18px] font-bold text-blue-500">
+            BUY THIS PROJECT{' '}
+            <span className=" text-[16px] font-bold text-slate-200">
+              for ₹{projectData.frontmatter.price}
+            </span>
+          </p>
+          <p className="mt-0 mb-4 text-sm text-gray-400">
+            Full Project + Installation Video + PPT + Docs
+          </p>
+        </div>
+
+        <h3 className="text-gray-300 font-extrabold mt-11 mb-4 text-2xl">OR</h3>
+
+        <div className="cursor-pointer bg-gray-900 rounded-lg shadow-3xl py-9 px-10 max-w-sm mx-auto border-green-500 border border-solid">
+          <p className="mb-0">
+            <a
+              href="/pro/"
+              className="font-display text-green-500 no-underline text-xl font-bold"
+            >
+              RESEARCH PAPER{' '} 
+            </a>
+            <span className=" text-[16px] font-bold text-slate-200">
+              for ₹{projectData.frontmatter.price+500}
+            </span>
+          </p>
+          <p className="mb-4 text-sm text-gray-400">
+            Everything + Research Paper + Plagiarism Report
+          </p>
+        </div>
+      </div>
+
       <Footer />
       <ScrollUp />
     </>
-  );
-};
+  )
+}
 
-export default ProjectPage;
+export default ProjectPage
--- projects/audiobook/_index.md
+++ projects/audiobook/_index.md
@@ -2,6 +2,7 @@
 title: AUDIOBOOK
 description: A tool to create audiobooks from text using text-to-speech.
 
+price: 499
 
 headline : Tool to create personalized audiobooks in your **own voice!** Add custom audio effects and background music to transform any text into an engaging auditory experience using [**RVC Real-Time Voice Cloning**](https://www.youtube.com/c/Fireship) and [**TTS Text-to-Speech**](https://www.youtube.com/c/Fireship). 
 
@@ -9,7 +10,7 @@ headline : Tool to create personalized audiobooks in your **own voice!** Add cus
 
 ---
 
-## 🧠 What will I Get?
+## 🎁 What will I Get?
 
 - 👨‍🎤 Write better code faster
 - 🎹 Edit code in rhythm with your keyboard
@@ -23,7 +24,7 @@ headline : Tool to create personalized audiobooks in your **own voice!** Add cus
 
 ## ⚡ Is the Course Updated?
 
-<span class="tag tag-sm tag-pro">Updated Nov 26th, 2021</span>
+Updated June 26th, 2024
 
 ## 🤔 Is this Course Right for Me?
 
@@ -32,4 +33,5 @@ This course is for beginners and experienced developers alike. It is fast-paced 
 ## How do I Buy?
 
 Just pick your favorite pack, scan the QR code to purchase, and join our private chat for the next steps. Let's make it happen!
+
 
